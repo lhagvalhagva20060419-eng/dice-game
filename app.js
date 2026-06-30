61972a6e68b13df45f222379c0bed23f9bebf7a7
lhagva <lhagvalhagva20060419@gmail.com>
2026-06-30 14:45:01 +0800

    projectiin ehnii huvisagchdiig uusgesen

---
+++ app.js
@@ -0,0 +1,17 @@
+//toglogchiin eeljiig hadgalah huvisagch,negdugeer toglogch  , 2r toglogch 1
+var activePlayer=1;
+//toglogchiin onoog tsugluulah huvisagch
+var scores=[0,0];
+
+//toglogchiin eeljindee tsugluulah onoog tsugluulah huvisagch
+var roundScore=0;
+//shoonii ali talaaraa buusniig hadgalah huvisagch
+var dice=Math.floor(Math.random()*6)+1;
+// program ehlehed beldii
+window.document.querySelector("#score-0").textContent=0;
+window.document.querySelector("#score-1").textContent=0;
+window.document.querySelector("#current-0").textContent=0;
+window.document.querySelector("#current-1").textContent=0;
+document.querySelector(".dice").style.display="none";
+
+
