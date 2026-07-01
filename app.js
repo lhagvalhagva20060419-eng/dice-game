71cbca8b7d252e5778764c6997f6bed97387caa6
lhagva <lhagvalhagva20060419@gmail.com>
2026-07-01 10:07:08 +0800

    shoog shiddeg bolgoh

--- app.js
+++ app.js
@@ -6,12 +6,21 @@ var scores=[0,0];
 //toglogchiin eeljindee tsugluulah onoog tsugluulah huvisagch
 var roundScore=0;
 //shoonii ali talaaraa buusniig hadgalah huvisagch
-var dice=Math.floor(Math.random()*6)+1;
+var diceNumber=Math.floor(Math.random()*6)+1;
 // program ehlehed beldii
-window.document.querySelector("#score-0").textContent=0;
-window.document.querySelector("#score-1").textContent=0;
-window.document.querySelector("#current-0").textContent=0;
-window.document.querySelector("#current-1").textContent=0;
-document.querySelector(".dice").style.display="none";
+document.getElementById("score-0").textContent='0';
+document.getElementById("score-1").textContent='0';
+document.getElementById("current-0").textContent='0';
+document.getElementById("current-1").textContent='0';
+
+var diceDom=document.querySelector(".dice");
+diceDom.style.display="none";
+document.querySelector(".btn-roll").addEventListener('click',function(){
+    var diceNumber=Math.floor(Math.random()*6)+1;   
+diceDom.style.display="block";
+diceDom.src="dice-"+diceNumber+".png";
+});
+
+
 
 
