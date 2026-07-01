9e2d0dbecb7cc8562bda6430756e484208c9bd0c
lhagva <lhagvalhagva20060419@gmail.com>
2026-07-01 12:07:56 +0800

    hold tovchiig ajilgaatai bolgono

--- app.js
+++ app.js
@@ -27,17 +27,33 @@ roundScore+=diceNumber;
 document.getElementById("current-"+activePlayer).textContent=roundScore;
 }else{
 document.getElementById("current-"+activePlayer).textContent=0;
-(activePlayer===0) ? activePlayer=1 : activePlayer=0;
+switchToNextPlayer();
+}
+});
+
+// hold tovchnii listener
+document.querySelector(".btn-hold").addEventListener("click",function(){
+    scores[activePlayer]+=roundScore;
+
+    document.getElementById("score-"+activePlayer).textContent=scores[activePlayer];
+    if(scores[activePlayer]>=10){
+        document.getElementById("name-"+activePlayer).textContent= "WINNER!!!";
+        document.querySelector(".player-"+activePlayer+"-panel").classList.add("winner");
+        document.querySelector(".player-"+activePlayer+"-panel").classList.remove("active");
+    }
+    else {
+     document.getElementById("current-"+activePlayer).textContent=0;
+    (activePlayer===0) ? activePlayer=1 : activePlayer=0;
+    switchToNextPlayer();
+    }
+   
+});
+
+function switchToNextPlayer(){
 roundScore=0;
 
 diceDom.style.display="none";
 
 document.querySelector(".player-"+(1-activePlayer)+"-panel").classList.remove("active");
 document.querySelector(".player-"+activePlayer+"-panel").classList.add("active");
-
 }
-});
-
-
-
-
